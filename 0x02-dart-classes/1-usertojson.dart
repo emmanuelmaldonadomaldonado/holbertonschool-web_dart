625700e001a8ef10ff9f41a0ee36b1839353c813
Emmanuel Maldonado <maldonadomaldonadoemmanuel227@gmail.com>
2025-03-09 15:06:48 -0400

    .

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -1,15 +1,11 @@
 class User {
-  String? name;
-  int? age;
-  double? height;
+  String name;
+  int age;
+  double height;
 
-  User({name: String, age: int, height: double}) {
-    this.name = name;
-    this.age = age;
-    this.height = height;
-  }
+  User({required this.name, required this.age, required this.height});
 
-  Map toJson() {
-    return {'name': this.name, 'age': this.age, 'height': this.height};
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'age': age, 'height': height};
   }
 }
