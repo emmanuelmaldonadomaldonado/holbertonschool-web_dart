a570a2a00663d7199292e3f41a5c595ea1eea045
Emmanuel Maldonado <maldonadomaldonadoemmanuel227@gmail.com>
2025-07-26 12:29:39 -0400

    Refactor calculateTotal function for improved readability and structure

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,24 +1,27 @@
-import 'dart:convert';
-import '4-util.dart';
+import "4-util.dart";
+import "dart:convert";
 
 Future<double> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
-    Map<String, dynamic> user = jsonDecode(userData);
-    String userId = user['id'];
-
-    String ordersJson = await fetchUserOrders(userId);
-    List<dynamic> orders = jsonDecode(ordersJson);
-
-    double total = 0;
-    for (var item in orders) {
-      String priceJson = await fetchProductPrice(item);
-      double price = jsonDecode(priceJson);
-      total += price;
-    }
-
-    return total;
-  } catch (error) {
+    // Call fetchUserData
+    return fetchUserData().then((user) {
+      // Convert string response to json
+      String id = json.decode(user)["id"];
+      // Asynchronously call fetchUserOrders with id of user from fetchUserData
+      return fetchUserOrders(id).then((products) async {
+        double totalPrice = 0.0;
+        // Convert string response to json and loop through product list
+        for (String product in json.decode(products)) {
+          // As each product is fetched, call fetchProductPrice with product
+          await fetchProductPrice(product).then((price) {
+            // Convert string response to json and add price to total
+            totalPrice += json.decode(price);
+          });
+        }
+        return totalPrice;
+      });
+    });
+  } catch (e) {
     return -1;
   }
 }
